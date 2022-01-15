33819858b3669db7e4dba2e688a410a465684d20
T3NED <software@t3ned.com>
2022-01-15 15:26:16 +0000

    feat: :sparkles: add module exports

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello, world!");
+// util
+export * as consts from "./util/constants";
+export * as util from "./util/functions";
+
+// cluster
+export * from "./cluster/Cluster";
+export * from "./cluster/ClusterManager";
+export * from "./cluster/ClusterLaunch";
+
+// ipc
+export * from "./ipc/IPC";
+export * from "./ipc/ClusterIPC";
+
+// struct
+export * from "./struct/Logger";
+export * from "./struct/Strategy";
