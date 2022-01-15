8235bd8b7b08ac31226623956bbc4da8f7ea1684
T3NED <software@t3ned.com>
2022-01-15 17:53:29 +0000

    feat: :sparkles: add built-in strategies for clustering, connecting and reconnecting

--- src/struct/Strategy.ts
+++ src/struct/Strategy.ts
@@ -1,4 +1,5 @@
-import type { ClusterManager } from "../index";
+import { ClusterManager, ClusterConfig, util } from "../index";
+import os from "os";
 
 export interface IClusterStrategy {
   /**
@@ -22,8 +23,9 @@ export interface IConnectStrategy {
   /**
    * Runs the strategy to connect the clusters
    * @param manager The manager running the strategy
+   * @param configs The cluster configs to connect
    */
-  run(manager: ClusterManager): Promise<void>;
+  run(manager: ClusterManager, configs: ClusterConfig[]): Promise<void>;
 }
 
 export interface IReconnectStrategy {
@@ -39,3 +41,116 @@ export interface IReconnectStrategy {
    */
   run(manager: ClusterManager, clusterId: number): Promise<void>;
 }
+
+/**
+ * The shared cluster strategy will assign shards to a single cluster
+ */
+export const sharedClusterStrategy = (): IClusterStrategy => {
+  return {
+    name: "shared",
+    run: async (manager) => {
+      const { clusterIdOffset, firstShardId, lastShardId } = manager;
+      await manager.fetchShardCount();
+
+      manager
+        .setCluster({
+          id: clusterIdOffset,
+          firstShardId,
+          lastShardId
+        })
+        .startCluster(clusterIdOffset);
+    }
+  };
+};
+
+/**
+ * The balanced cluster strategy will assign shards equally across CPU cores
+ */
+export const balancedClusterStrategy = (): IClusterStrategy => {
+  return {
+    name: "balanced",
+    run: customClusterStrategy(os.cpus().length).run
+  };
+};
+
+/**
+ * The custom cluster strategy will assign shards across the specified number of clusters
+ * @param clusterCount The number of clusters to start
+ * @param maxShardsPerCluster The maximum number of shards per cluster
+ */
+export const customClusterStrategy = (
+  clusterCount: number,
+  maxShardsPerCluster = 0
+): IClusterStrategy => {
+  return {
+    name: "custom",
+    run: async (manager) => {
+      const { clusterIdOffset, firstShardId, lastShardId } = manager;
+      const shardCount = await manager.fetchShardCount();
+
+      if (maxShardsPerCluster) {
+        // Ensure the shard count is not larger than the max shards
+        const maxTotalShards = clusterCount * maxShardsPerCluster;
+        if (maxTotalShards < shardCount) {
+          throw new Error(
+            "Invalid `shardsPerCluster` provided. `shardCount` should be >= `clusterCount` * `shardsPerCluster`"
+          );
+        }
+      }
+
+      const shardChunks = util.chunkShards(clusterCount, firstShardId, lastShardId, true);
+
+      for (let i = 0; i < shardChunks.length; i++) {
+        const shardChunk = shardChunks[i];
+
+        manager
+          .setCluster({
+            id: clusterIdOffset + i,
+            firstShardId: shardChunk[0],
+            lastShardId: shardChunk[shardChunk.length - 1]
+          })
+          .startCluster(clusterIdOffset + i);
+      }
+    }
+  };
+};
+
+/**
+ * The ordered connect strategy will connect the clusters in the order they were set
+ */
+export const orderedConnectStrategy = (): IConnectStrategy => {
+  return {
+    name: "ordered",
+    run: async (manager, clusters) => {
+      const callback = (cluster: ClusterConfig) => {
+        const { id, shardCount } = cluster;
+        manager.logger.info(`[C${id}] Connecting with ${shardCount} shards`);
+
+        // Unload event once all clusters have connected
+        if (cluster.id === clusters[clusters.length - 1].id) {
+          manager.queue.off("connectCluster", callback);
+        }
+      };
+
+      manager.queue.on("connectCluster", callback);
+
+      for (const cluster of clusters) {
+        // Push the clusters into the queue
+        manager.queue.enqueue(cluster);
+      }
+    }
+  };
+};
+
+/**
+ * The queued reconnect strategy will reconnect the clusters bu adding them to the queue
+ */
+export const queuedReconnectStrategy = (): IReconnectStrategy => {
+  return {
+    name: "queued",
+    run: async (manager, clusterId) => {
+      const cluster = manager.getCluster(clusterId);
+      if (cluster) return orderedConnectStrategy().run(manager, [cluster]);
+    }
+  };
+};
--- src/util/functions.ts
+++ src/util/functions.ts
@@ -8,3 +8,42 @@ import { randomBytes } from "crypto";
 export const generateIPCFetchId = (op: IPCMessageOp): string => {
   return `ipc.${op}.${randomBytes(16).toString("hex")}`;
 };
+
+/**
+ * Splits an array into N number of chunks
+ * @param chunkCount The number of chunks to create
+ * @param array The array to chunk
+ */
+export const chunkArray = <T = any>(chunkCount: number, array: T[]): T[][] => {
+  const chunked: T[][] = [];
+
+  for (let i = chunkCount; i > 0; i--) {
+    chunked.push(array.splice(0, Math.ceil(array.length / i)));
+  }
+
+  return chunked;
+};
+
+/**
+ * Splits the shards into evenly spread chunks
+ * @param clusterCount The number of clusters
+ * @param firstShardId The id of the first shard
+ * @param lastShardId The id of the last shard
+ * @param discardEmpty Whether or not to discard empty chunks
+ */
+export const chunkShards = (
+  clusterCount: number,
+  firstShardId: number,
+  lastShardId: number,
+  discardEmpty = false
+): number[][] => {
+  const shards: number[] = [];
+
+  // Fill the shards array with shard Ids from firstShardId to lastShardId
+  for (let i = firstShardId; i <= lastShardId; i++) shards.push(i);
+
+  const chunks = chunkArray(clusterCount, shards);
+  if (!discardEmpty) return chunks;
+
+  return chunks.filter((chunk) => chunk.length !== 0);
+};
